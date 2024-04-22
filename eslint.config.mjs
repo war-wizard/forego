f3c8a80076eccb6fd2720ca27a34fd7fcd2438bd
war-wizard <63116579+war-wizard@users.noreply.github.com>
2024-04-22 16:36:06 -0300

    add eslint style rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default tseslint.config({
   files: ['src/**/*.ts'],
   extends: [
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
+    stylistic.configs.customize({
+      semi: true,
+      braceStyle: '1tbs',
+      quoteProps: 'as-needed',
+    }),
   ],
+  rules: {
+    '@stylistic/padded-blocks': 'off',
+  },
 });
